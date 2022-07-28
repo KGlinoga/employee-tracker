b4e704ae3f9cfadaa0835f16389c7e76cb9c1577
Kristina Faye <kglinoga@gmail.com>
2022-07-28 09:49:10 -0700

    basics for index.js and schema.sql imported.

---
+++ index.js
@@ -0,0 +1,38 @@
+// importing packages
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+// dynamic port
+const PORT = process.env.PORT || 3001;
+// instance of express
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// connection to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'kglinoga',
+        password: '',
+        database:'business_db'
+    },
+    console.log(`Connected to the business_db database.`)
+);
+
+// query database placeholder for initial code build
+db.query('SELECT * FROM depts', function (err, results){
+    console.log(results);
+});
+
+// default responst for other requests
+app.use((req,res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
